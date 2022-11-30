c515c253d9489b19638b1a60bc20f2ae9ed232f2
Walter Thomas <walterdthomas@me.com>
2022-11-30 18:50:06 -0500

    walter

---
+++ furniture-depot/src/components/Outside.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+export default function Outside () {
+
+const [outside, setOutside] = useState(null)
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get(`https://www.furniture.com`)
+            setOutside(response.data.outside)
+        }
+        getData()
+    }, [])
+    if (!outside) {
+        return <h2> LOADING PAGE </h2>
+    } else {
+        return (
+            <div className="container">
+            <div className="outsfurn">
+
+
+            </div>    
+            </div>
+        )
+    }    
+    
+}
