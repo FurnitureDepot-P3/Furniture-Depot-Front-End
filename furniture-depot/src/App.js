b27c9d5f11fc8411b750c23257830f44597dbc96
Walter Thomas <walterdthomas@me.com>
2022-12-01 14:32:48 -0500

    walter

--- furniture-depot/src/App.js
+++ furniture-depot/src/App.js
@@ -1,1 +1,26 @@
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header />
+
+      
+
+        <Main />
+
+
+
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
