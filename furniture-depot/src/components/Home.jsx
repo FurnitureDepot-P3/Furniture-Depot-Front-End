6bf9c3142faacf4d17cb353f4b3c227d7a910623
hamchops <martinezlerenzo@gmail.com>
2022-12-04 16:59:36 -0600

    main 4 button are responsive on home page. Need to add images.

--- furniture-depot/src/components/Home.jsx
+++ furniture-depot/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SearchForm from './SearchForm'
 import Nav from './Nav'
+import { Link } from 'react-router-dom'
 
 export default function Home (props) {
     return (
@@ -11,20 +12,24 @@ export default function Home (props) {
             </div>
 
         <div className="catbtns">
-                <button className="livingbtn">
-                Living Room
+                <button className="livingbtn"> <Link to='/Categories/2' style={{textDecoration: 'none'}}>
+                    Living Room
+                </Link>
                 </button>
 
-                <button className="diningbtn">
-                Dining Room
+                <button className="diningbtn">  <Link to='/Dining' style={{textDecoration: 'none'}}>
+                    DINING ROOM
+                </Link>
                 </button>
 
-                <button className="bedbtn">
-                Bedroom
+                <button className="bedbtn"> <Link to='/Categories/1' style={{textDecoration: 'none'}}>
+                    BEDROOM
+                </Link>
                 </button>
 
-                <button className="outbtn">
-                Outdoor Furniture
+                <button className="outbtn"> <Link to='/Outside' style={{textDecoration: 'none'}}>
+                    OUTSIDE FURNITURE
+                </Link>
                 </button>
         </div>
 
