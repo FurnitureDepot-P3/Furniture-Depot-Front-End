7b14d2198cb16822bba4f52938ac2dfa1710ca25
Matt <mkiska1@gmail.com>
2022-12-05 10:42:56 -0500

    added auth services, updated signin/register

--- furniture-depot/src/components/SignIn.jsx
+++ furniture-depot/src/components/SignIn.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { RegisterUser, SignInUser } from '../services/Auth'
+
 
 function SignIn(props) {
 
