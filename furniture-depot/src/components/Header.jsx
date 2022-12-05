90b14a39a9a23cb43db292de3af624e0649280a9
Walter Thomas <walterdthomas@me.com>
2022-12-05 12:08:52 -0500

    sign in and register in header

--- furniture-depot/src/components/Header.jsx
+++ furniture-depot/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Register from './Register'
 import SearchForm from "./SearchForm"
 import SignIn from './SignIn'
+import { Link } from "react-router-dom"
 
 export default function Header () {
     return (
         <div id="containerhdr">
-            <h2 className='signin'> <SignIn/> </h2>
+            <div className="signinbox">
+            <Link id="signinbtn" to='/SignIn' style={{textDecoration: 'none'}}>Sign In</Link> or <Link id="registerbtn" to="/Register" style={{textDecoration: 'none'}}>Register</Link>
+            
+            </div>
             <h2 className="headertitle"> Furniture header </h2>
             <h2 className="searchbox" > <SearchForm /> </h2>
         </div>
