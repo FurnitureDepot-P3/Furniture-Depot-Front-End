16ecb6b9956056a40957d58ee1cffdead00ef461
Matt Kiska <114313083+mattkiska@users.noreply.github.com>
2022-12-05 10:20:10 -0500

    Revert "updated register"

--- furniture-depot/src/components/SignIn.jsx
+++ furniture-depot/src/components/SignIn.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
 
 function SignIn(props) {
 
@@ -20,38 +19,9 @@ function SignIn(props) {
       props.toggleAuthenticated(true)
       navigate('/Home')
     }
-
+    
   return (
-    <div className="signin col">
-      <div className="card-overlay centered">
-        <form className="col" onSubmit={handleSubmit}>
-          <div className="input-wrapper">
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={handleChange}
-              name="email"
-              type="email"
-              placeholder="example@example.com"
-              value={formValues.email}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              name="password"
-              value={formValues.password}
-              required
-            />
-          </div>
-          <button disabled={!formValues.email || !formValues.password}>
-            Sign In
-          </button>
-        </form>
-      </div>
-    </div>
+    <div>SignIn</div>
   )
 }
 
