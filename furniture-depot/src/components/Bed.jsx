013913d316ef6d64a7ec50f93d990f45731047eb
Jason Carmichael <jasonc0308@icloud.com>
2022-12-06 19:01:06 -0600

    reformatting review placement on cards

--- furniture-depot/src/components/Bed.jsx
+++ furniture-depot/src/components/Bed.jsx
@@ -21,10 +21,11 @@ const [review, setReview] = useState(null)
     }, [])
     return bedroom && review ? (
         <div className="container">
+            <Link to="/" className="back-btn" id="home-btn"> ◁ Home </Link>
 
-            <div className="product-card">  
+            <div className="product-container">  
                 {bedroom.map((products) =>(
-                    <div className="box" key={products.name}>
+                    <div className="product-card" key={products.name}>
                         <h3 className="productline1"> {products.name}</h3>
                         <h2 className="price">${products.price}</h2>
                         <p>In stock. Ships today.</p>
@@ -32,14 +33,19 @@ const [review, setReview] = useState(null)
                         <h6>DESCRIPTION</h6>
                         <p>{products.description}</p>
                         <h6>REVIEWS</h6>
+                        <p className="review-text"> People who purchased this product rate it a {review.reviews[0].rating} out of 5!</p>
+                        <p className="review-text">{review.reviews[0].comment} </p>
+                        <div className="review-link">
+                            <Link to="/Reviews" className="review-link"> Write a Review </Link>
+                        </div>
                         </div>
                 ))}
-            <div className="review-card">
+            {/* <div className="review-card">
                     <div className="box" >
                         <h3 className="productline1">{review.reviews[0].comment} </h3>
                         <h4 className="rating">People who purchased this product rate it a {review.reviews[0].rating} out of 5!</h4>
                     </div>
-            </div>
+            </div> */}
             </div>
         </div>
         ) : <h1> Loading Please Wait ... </h1>
