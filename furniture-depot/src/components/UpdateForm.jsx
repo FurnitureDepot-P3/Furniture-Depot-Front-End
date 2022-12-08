eef7cd9c5707e56c8cc02fac339c06d676222160
Jason Carmichael <jasonc0308@icloud.com>
2022-12-08 11:57:51 -0600

    UpdateReview component nested in UpdateForm

--- furniture-depot/src/components/UpdateForm.jsx
+++ furniture-depot/src/components/UpdateForm.jsx
@@ -1,6 +1,11 @@
+import UpdateReview from "./UpdateReview"
+
 export default function UpdateForm() {
 
     return (
+        <div className="update-container">
         <h2>Edit Your Review</h2>
+        <UpdateReview />
+        </div>
     )
 }
