dee49a7956bbcf2974818ea7445fb160e1a7889b
Walter Thomas <walterdthomas@me.com>
2022-12-08 09:25:29 -0500

    walter

--- furniture-depot/src/components/Outside.jsx
+++ furniture-depot/src/components/Outside.jsx
@@ -30,7 +30,7 @@ const [review, setReview] = useState(null)
                         <h3 className="productline1"> {products.name}</h3>
                         <h2 className="price">${products.price}</h2>
                         <p>In stock. Ships today.</p>
-                        <img src={products.image} />
+                        <img src={products.image} alt="#"/>
                         <h6>DESCRIPTION</h6>
                         <p>{products.description}</p>
                         <h6>PRODUCT #</h6>
